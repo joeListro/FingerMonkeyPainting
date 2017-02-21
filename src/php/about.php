59c1b3dea7764df42917ad959307b870a55a2228
Joe Listro <jlistro@gmail.com>
2017-02-21 14:22:57 -0500

    About Me stuff added. Still rip ScrumMasterSteve.

--- src/php/about.php
+++ src/php/about.php
@@ -25,7 +25,51 @@ echo '
         <!--end nav bar-->
 
         <h1>About Ladder Monkey Painting</h1>
+		
     </div>
+	
+	<div class="page-header">
+		<h1>About Us</h1>
+	</div>
+	
+	<div class="well">
+
+		Ladder Monkey Painting creates a holistic approach to house maintenance and client satisfaction. An expert in the field with residential customers, and offerings for light commercial potential. We provide interior and exterior painting abilities that continuously exceed expectations.
+
+	</div>
+	
+	<div class="page-header">
+		<h1>Contact Me</h1>
+	</div>
+
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Name</h3>
+		</div>
+		<div class="panel-body">
+			John Clark
+		</div>
+	</div>
+	
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Phone</h3>
+		</div>
+		<div class="panel-body">
+			(603) 425-4684
+		</div>
+	</div>
+	
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Address</h3>
+		</div>
+		<div class="panel-body">
+			<p>242 South Winooski Ave</p>
+			<p>Burlington, VT 05401</p>
+		</div>
+	</div>
+		
 </div>';
 
 include("../include/footer.inc");
